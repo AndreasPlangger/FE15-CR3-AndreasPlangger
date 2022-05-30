3f1241f8b09372054edf7375f282aede2d3df6eb
AndreasPlangger <andreas_plangger@hotmail.com>
2022-05-31 00:58:32 +0200

    clean and simplify code

--- FE15-CR3-AndreasPlangger/js/script.js
+++ FE15-CR3-AndreasPlangger/js/script.js
@@ -17,19 +17,19 @@ console.log(calculateInvoice(art2, art5, art8));
 console.log(calculateInvoice(art3, art6, art9));
 
 
-console.log("\n"+"\n"+"\n"+"\n");
+console.log("\n" + "\n" + "\n" + "\n");
 
 
 function studentInvoice(stage1, stage2, stage3,) {
     var invoice = stage1 + stage2 + stage3;
-    var invRed = invoice*0.9
+    var invRed = invoice * 0.9
     var invRound = invRed.toFixed(2);
 
 
-    return ("Your current invoice amounts to a total of: € " + invoice + "."+"\n"+"\n"+"\n"+"However,"+"\n"+ "because you are a student..."+"\n"+"\n"+ "and we are nice people..." +"\n"+"\n"+"We grant you a 10% reduction!"+"\n"+"\n"+"Your reduced student invoice amounts to a total of: € " + invRound + ",--")
+    return ("Your current invoice amounts to a total of: € " + invoice + "." + "\n" + "\n" + "\n" + "However," + "\n" + "because you are a student..." + "\n" + "\n" + "and we are nice people..." + "\n" + "\n" + "We grant you a 10% reduction!" + "\n" + "\n" + "Your reduced student invoice amounts to a total of: € " + invRound + ",--")
 }
 console.log(studentInvoice(art1, art4, art7));
-console.log("\n"+"\n"+"\n"+"\n");
+console.log("\n" + "\n" + "\n" + "\n");
 console.log(studentInvoice(art2, art5, art8));
-console.log("\n"+"\n"+"\n"+"\n");
+console.log("\n" + "\n" + "\n" + "\n");
 console.log(studentInvoice(art3, art6, art9));
